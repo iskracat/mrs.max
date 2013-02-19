975c81963f17874e6a3779ba27905d51c2d56f1b
Victor Fernandez de Alba <sneridagh@gmail.com>
2013-02-19 15:55:06 +0100

    Refinements to configlet

--- mrs/max/browser/controlpanel.py
+++ mrs/max/browser/controlpanel.py
@@ -19,12 +19,10 @@ from maxclient import MaxClient
 from plone.directives import form
 
 
-DEFAULT_OAUTH_TOKEN_ENDPOINT = 'https://oauth.upc.edu/token'
-DEFAULT_OAUTH_GRANT_TYPE = 'password'
-DEFAULT_MAX_SERVER = 'https://max.upc.edu'
-DEFAULT_MAX_APP_USERNAME = 'appusername'
-DEFAULT_MAX_APP_DISPLAYNAME = 'App Display Name'
-DEFAULT_MAX_APP_PASSWORD = 'apppass'
+DEFAULT_OAUTH_TOKEN_ENDPOINT = u'https://oauth.upc.edu'
+DEFAULT_OAUTH_GRANT_TYPE = u'password'
+DEFAULT_MAX_SERVER = u'https://max.upc.edu'
+DEFAULT_MAX_APP_USERNAME = u'appusername'
 
 
 class IMAXUISettings(form.Schema):
@@ -32,16 +30,15 @@ class IMAXUISettings(form.Schema):
     configuration registry and obtainable via plone.registry.
     """
 
-    oauth_token_endpoint = schema.ASCIILine(
-        title=_(u'label_oauth_token_endpoint', default=u'OAuth token endpoint'),
-        description=_(u'help_oauth_token_endpoint',
-                        default=u"Please, specify the URI for the oAuth token "
-                                u"endpoint."),
+    oauth_server = schema.TextLine(
+        title=_(u'label_oauth_server', default=u'OAuth token endpoint'),
+        description=_(u'help_oauth_server',
+                        default=u"Please, specify the URI for the oAuth server."),
         required=True,
         default=DEFAULT_OAUTH_TOKEN_ENDPOINT
         )
 
-    oauth_grant_type = schema.ASCIILine(
+    oauth_grant_type = schema.TextLine(
         title=_(u'label_oauth_grant_type', default=u'OAuth grant type'),
         description=_(u'help_oauth_grant_type',
                         default=u"Please, specify the oAuth grant type."),
@@ -49,7 +46,7 @@ class IMAXUISettings(form.Schema):
         default=DEFAULT_OAUTH_GRANT_TYPE
         )
 
-    max_server = schema.ASCIILine(
+    max_server = schema.TextLine(
         title=_(u'label_max_server', default=u'MAX Server URL'),
         description=_(u'help_max_server',
                         default=u"Please, specify the MAX Server URL."),
@@ -57,15 +54,16 @@ class IMAXUISettings(form.Schema):
         default=DEFAULT_MAX_SERVER
         )
 
-    max_server_alias = schema.ASCIILine(
+    max_server_alias = schema.TextLine(
         title=_(u'label_max_server_alias', default=u'MAX Server URL Alias (Fallback when no CORS available)'),
         description=_(u'help_max_server_alias',
                         default=u"Please, specify the MAX Server URL Alias."),
-        required=True,
+        required=False,
         default=DEFAULT_MAX_SERVER
         )
 
-    max_app_username = schema.ASCIILine(
+    form.mode(max_app_username='hidden')
+    max_app_username = schema.TextLine(
         title=_(u'label_max_app_username', default=u'MAX application agent username'),
         description=_(u'help_max_app_username',
                         default=u"Please, specify the MAX application agent username."),
@@ -73,27 +71,27 @@ class IMAXUISettings(form.Schema):
         default=DEFAULT_MAX_APP_USERNAME
         )
 
-    max_app_displayname = schema.ASCIILine(
-        title=_(u'label_max_app_displayname', default=u'MAX application agent display Name'),
-        description=_(u'help_max_app_displayname',
-                        default=u"Please, specify the MAX application agent display Name."),
-        required=False,
-        default=DEFAULT_MAX_APP_USERNAME
-        )
-
-    max_app_password = schema.ASCIILine(
-        title=_(u'label_max_app_password', default=u'MAX application agent password'),
-        description=_(u'help_max_app_password',
-                        default=u"Please, specify the MAX application agent password."),
-        required=False,
-        default=DEFAULT_MAX_APP_PASSWORD
-        )
-
     form.mode(max_app_token='hidden')
-    max_app_token = schema.ASCIILine(
+    max_app_token = schema.Password(
         title=_(u'label_max_app_token', default=u'MAX application token'),
         description=_(u'help_max_app_token',
                         default=u"Please, specify the MAX application token."),
+        required=False,
+        )
+
+    form.mode(max_restricted_username='hidden')
+    max_restricted_username = schema.TextLine(
+        title=_(u'label_max_restricted_username', default=u'MAX restricted username'),
+        description=_(u'help_max_restricted_username',
+                        default=u"Please, specify the MAX restricted username."),
+        required=False,
+        )
+
+    form.mode(max_restricted_token='hidden')
+    max_restricted_token = schema.Password(
+        title=_(u'label_max_restricted_token', default=u'MAX restricted user token'),
+        description=_(u'help_max_restricted_token',
+                        default=u"Please, specify the MAX restricted user token."),
         required=False,
         )
 
@@ -110,7 +108,7 @@ class MAXUISettingsEditForm(controlpanel.RegistryEditForm):
 
     def updateFields(self):
         super(MAXUISettingsEditForm, self).updateFields()
-        self.fields = self.fields.omit('max_app_token')
+        #self.fields = self.fields.omit('max_app_token')
 
     def updateWidgets(self):
         super(MAXUISettingsEditForm, self).updateWidgets()
@@ -133,38 +131,38 @@ class MAXUISettingsEditForm(controlpanel.RegistryEditForm):
         self.request.response.redirect("%s/%s" % (self.context.absolute_url(),
                                                   self.control_panel_view))
 
-    @button.buttonAndHandler(_(u'Get token'), name='getToken')
-    def handleGetToken(self, action):
-        data, errors = self.extractData()
-        credentials = dict(login=data.get('max_app_username'),
-                           password=data.get('max_app_password'))
-        from upc.maxui.max import getToken
-        logger = logging.getLogger('upc.maxui')
+    # @button.buttonAndHandler(_(u'Get token'), name='getToken')
+    # def handleGetToken(self, action):
+    #     data, errors = self.extractData()
+    #     credentials = dict(login=data.get('max_app_username'),
+    #                        password=data.get('max_app_password'))
+    #     from upc.maxui.max import getToken
+    #     logger = logging.getLogger('upc.maxui')
 
-        #Authenticat to max as operations user
-        maxcli = MaxClient(data.get('max_server'), auth_method="basic")
-        maxcli.setBasicAuth(data.get('max_ops_username'), data.get('max_ops_password'))
+    #     #Authenticat to max as operations user
+    #     maxcli = MaxClient(data.get('max_server'), auth_method="basic")
+    #     maxcli.setBasicAuth(data.get('max_ops_username'), data.get('max_ops_password'))
 
-        #Add App user to max
-        result = maxcli.addUser(credentials['login'], displayName=data.get('max_app_displayname'))
-        if not result:
-            logger.info('Error creating MAX user for user: %s' % credentials['login'])
-            IStatusMessage(self.request).addStatusMessage(_(u"An error occurred during creation of max user"), "info")
-        else:
-            logger.info('MAX Agent user %s created' % credentials['login'])
-            # Request token for app user
-            oauth_token = getToken(credentials)
-            registry = queryUtility(IRegistry)
-            settings = registry.forInterface(IMAXUISettings, check=False)
-            settings.max_app_token = str(oauth_token)
+    #     #Add App user to max
+    #     result = maxcli.addUser(credentials['login'], displayName=data.get('max_app_displayname'))
+    #     if not result:
+    #         logger.info('Error creating MAX user for user: %s' % credentials['login'])
+    #         IStatusMessage(self.request).addStatusMessage(_(u"An error occurred during creation of max user"), "info")
+    #     else:
+    #         logger.info('MAX Agent user %s created' % credentials['login'])
+    #         # Request token for app user
+    #         oauth_token = getToken(credentials)
+    #         registry = queryUtility(IRegistry)
+    #         settings = registry.forInterface(IMAXUISettings, check=False)
+    #         settings.max_app_token = str(oauth_token)
 
-            #Subscribe app user to max
-            club_url = getToolByName(self.context, "portal_url").getPortalObject().absolute_url()
-            maxcli.setActor(credentials['login'])
-            maxcli.subscribe(club_url)
+    #         #Subscribe app user to max
+    #         club_url = getToolByName(self.context, "portal_url").getPortalObject().absolute_url()
+    #         maxcli.setActor(credentials['login'])
+    #         maxcli.subscribe(club_url)
 
-            logger.info('MAX user %s subscribed to %s' % (credentials['login'], club_url))
-            IStatusMessage(self.request).addStatusMessage(_(u"Token for MAX application user saved"), "info")
+    #         logger.info('MAX user %s subscribed to %s' % (credentials['login'], club_url))
+    #         IStatusMessage(self.request).addStatusMessage(_(u"Token for MAX application user saved"), "info")
 
 
 class MAXUISettingsControlPanel(controlpanel.ControlPanelFormWrapper):
@@ -172,3 +170,14 @@ class MAXUISettingsControlPanel(controlpanel.ControlPanelFormWrapper):
     """
     form = MAXUISettingsEditForm
     index = ViewPageTemplateFile('controlpanel.pt')
+
+    def update(self):
+        registry = queryUtility(IRegistry)
+        self.maxui_settings = registry.forInterface(IMAXUISettings)
+        super(MAXUISettingsControlPanel, self).update()
+
+    def restricted_token_class(self):
+        return self.maxui_settings.max_restricted_token and 'info' or 'warning'
+
+    def app_token_class(self):
+        return self.maxui_settings.max_app_token and 'info' or 'warning'
--- mrs/max/browser/tokens.py
+++ mrs/max/browser/tokens.py
@@ -3,18 +3,23 @@ from plone.directives import form
 
 from zope import schema
 from z3c.form import button
+from zope.component import queryUtility
+
+from plone.registry.interfaces import IRegistry
 
 from Products.CMFCore.interfaces import ISiteRoot
 from Products.statusmessages.interfaces import IStatusMessage
 
 from mrs.max import MRSMAXMessageFactory as _
+from maxclient import MaxClient
+from mrs.max.browser.controlpanel import IMAXUISettings
 
 grok.templatedir('views_templates')
 
 
 class ICredentials(form.Schema):
 
-    username = schema.ASCIILine(
+    username = schema.TextLine(
             title=_(u"The username."),
         )
 
@@ -25,9 +30,7 @@ class ICredentials(form.Schema):
 
 
 class getTokenForm(form.SchemaForm):
-    grok.name('getToken')
-    grok.require('cmf.ManagePortal')
-    grok.context(ISiteRoot)
+    grok.baseclass()
 
     schema = ICredentials
     ignoreContext = True
@@ -39,8 +42,18 @@ class getTokenForm(form.SchemaForm):
         # disable Plone's editable border
         self.request.set('disable_border', True)
 
+        registry = queryUtility(IRegistry)
+        self.maxui_settings = registry.forInterface(IMAXUISettings)
+
         # call the base class version - this is very important!
         super(getTokenForm, self).update()
+
+
+class getRestrictedTokenForm(getTokenForm):
+    grok.name('getRestrictedToken')
+    grok.require('cmf.ManagePortal')
+    grok.template('gettokenform')
+    grok.context(ISiteRoot)
 
     @button.buttonAndHandler(_(u'Get token'))
     def handleApply(self, action):
@@ -56,12 +69,55 @@ class getTokenForm(form.SchemaForm):
         username = data['username']
         password = data['password']
 
+        maxclient = MaxClient(self.maxui_settings.max_server, self.maxui_settings.oauth_server)
+        self.maxui_settings.max_restricted_username = username
+
+        try:
+            self.maxui_settings.max_restricted_token = maxclient.getToken(username, password)
+            IStatusMessage(self.request).addStatusMessage(
+                "Restricted token issued for user: {}".format(username),
+                "info")
+        except AttributeError, error:
+            IStatusMessage(self.request).addStatusMessage(
+                error,
+                "error")
+
         # Redirect back to the front page with a status message
+        self.request.response.redirect("{}/{}".format(self.context.absolute_url(), '@@maxui-settings'))
 
-        IStatusMessage(self.request).addStatusMessage(
-                _(u"Thank you for your order. We will contact you shortly"),
-                "info"
-            )
 
-        contextURL = self.context.absolute_url()
-        self.request.response.redirect(contextURL)
+class getAppTokenForm(getTokenForm):
+    grok.name('getAppToken')
+    grok.require('cmf.ManagePortal')
+    grok.template('gettokenform')
+    grok.context(ISiteRoot)
+
+    @button.buttonAndHandler(_(u'Get token'))
+    def handleApply(self, action):
+        data, errors = self.extractData()
+        if errors:
+            self.status = self.formErrorsMessage
+            return
+
+        # Handle order here. For now, just print it to the console. A more
+        # realistic action would be to send the order to another system, send
+        # an email, or similar
+
+        username = data['username']
+        password = data['password']
+
+        maxclient = MaxClient(self.maxui_settings.max_server, self.maxui_settings.oauth_server)
+        self.maxui_settings.max_app_username = username
+
+        try:
+            self.maxui_settings.max_app_token = maxclient.getToken(username, password)
+            IStatusMessage(self.request).addStatusMessage(
+                "App token issued for user: {}".format(username),
+                "info")
+        except AttributeError, error:
+            IStatusMessage(self.request).addStatusMessage(
+                error,
+                "error")
+
+        # Redirect back to the front page with a status message
+        self.request.response.redirect("{}/{}".format(self.context.absolute_url(), '@@maxui-settings'))
