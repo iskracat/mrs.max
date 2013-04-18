92d040d83d427695f0300632b3ac55f871c28c8b
victorfda <victor.fernandez@upcnet.es>
2013-04-18 16:32:12 +0200

    Sort activities by comment date

--- mrs/max/static/maxui.loader.js
+++ mrs/max/static/maxui.loader.js
@@ -18,7 +18,8 @@ window._MAXUI.onReady = function() {
                 'avatarURLpattern' : _MAXUI.avatar_url,
                 'readContext' : _MAXUI.contexts,
                 'activitySource': _MAXUI.activitySource,
-                'maxTalkURL': _MAXUI.max_server
+                'maxTalkURL': _MAXUI.max_server,
+                'activitySortOrder': 'comments'
                }
 
     $('#maxui-widget-container').maxUI(settings)
