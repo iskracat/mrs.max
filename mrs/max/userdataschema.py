ace095a1d49beb3664b74bf0481123aeb2ed0447
Victor Fernandez de Alba <sneridagh@gmail.com>
2013-10-01 15:50:11 +0200

    Override portrait user information

--- mrs/max/userdataschema.py
+++ mrs/max/userdataschema.py
@@ -3,6 +3,8 @@ from zope.interface import implements
 from plone.app.users.userdataschema import IUserDataSchema
 from plone.app.users.userdataschema import IUserDataSchemaProvider
 from plone.app.users.browser.personalpreferences import UserDataPanelAdapter
+from Products.CMFDefault.formlib.schema import FileUpload
+from Products.CMFPlone import PloneMessageFactory as PLMF
 
 from mrs.max import MRSMAXMessageFactory as _
 
@@ -11,6 +13,17 @@ class IEnhancedUserDataSchema(IUserDataSchema):
     """ Use all the fields from the default user data schema, and add various
     extra fields.
     """
+    portrait = FileUpload(title=PLMF(u'label_portrait', default=u'Portrait'),
+        description=_(u'help_portrait',
+                      default=u'To add or change the portrait: click the '
+                      '"Browse" button; select a picture of yourself.'),
+        required=False)
+
+    pdelete = schema.Bool(
+        title=PLMF(u'label_delete_portrait', default=u'Delete Portrait'),
+        description=u'',
+        required=False)
+
     twitter_username = schema.TextLine(
         title=_(u'label_twitter', default=u'Twitter username'),
         description=_(u'help_twitter',
