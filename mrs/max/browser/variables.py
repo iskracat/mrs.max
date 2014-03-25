3a166f65830e108988e0db06fa2425c570c4983d
Victor Fernandez de Alba <sneridagh@gmail.com>
2014-03-25 21:15:36 +0100

    Admin user impersonate as restricted user

--- mrs/max/browser/variables.py
+++ mrs/max/browser/variables.py
@@ -1,10 +1,8 @@
-from zope.i18n import translate
 from zope.component import queryUtility
 from zope.component.hooks import getSite
 from zope.publisher.browser import BrowserView
 
 from Products.CMFCore.utils import getToolByName
-from Products.CMFPlone import PloneMessageFactory as _
 
 from plone.registry.interfaces import IRegistry
 
@@ -23,15 +21,6 @@ window._MAXUI.contexts = '%(contexts)s';
 window._MAXUI.activitySource = '%(activitySource)s';
 window._MAXUI.language = '%(language)s';
 """
-
-FORM_MODIFIED = _(u'text_form_modified_message',
-    default=u'Your form has not been saved. All changes you have made will be lost.')
-
-FORM_RESUBMIT = _(u'text_form_resubmit_message',
-    default=u'You already clicked the submit button. Do you really want to submit this form again?')
-
-AJAX_NORESPONSE = _(u'text_ajax_noresponse_message',
-    default=u'No response from server. Please try again later.')
 
 
 class MAXJSVariables(BrowserView):
@@ -56,15 +45,10 @@ class MAXJSVariables(BrowserView):
             member = pm.getMemberById(username)
             oauth_token = member.getProperty('oauth_token', None)
 
-        # Per si de cas les necessitem
-        # form_modified = translate(FORM_MODIFIED, context=self.request)
-        # form_resubmit = translate(FORM_RESUBMIT, context=self.request)
-        # ajax_noresponse = translate(AJAX_NORESPONSE, context=self.request)
-
-        # # escape_for_js
-        # form_modified = form_modified.replace("'", "\\'")
-        # form_resubmit = form_resubmit.replace("'", "\\'")
-        # ajax_noresponse = ajax_noresponse.replace("'", "\\'")
+        # Use the restricted username and token in case we are admin.
+        if username == 'admin':
+            username = settings.max_restricted_username
+            oauth_token = settings.max_restricted_token
 
         pl = getToolByName(self.context, "portal_languages")
         default_lang = pl.getDefaultLanguage()
