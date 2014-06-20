668dae03b40077c586fac0d7d12785c546807948
Victor Fernandez de Alba <sneridagh@gmail.com>
2014-06-20 12:29:44 +0200

    Update mrs max way of doing things and fix a major error when creating user on MAX

--- mrs/max/auth.py
+++ mrs/max/auth.py
@@ -1,13 +1,13 @@
-from Products.CMFCore.utils import getToolByName
-from pas.plugins.preauth.interfaces import IPreauthTask, IPreauthHelper
-from plone.registry.interfaces import IRegistry
-from zope.component import queryUtility
+from plone import api
+from pas.plugins.preauth.interfaces import IPreauthTask
+from pas.plugins.preauth.interfaces import IPreauthHelper
+
+from zope.component import getUtility
 from zope.interface import implements
 from zope.component import adapts
 
-from maxclient.rest import MaxClient
-from mrs.max.browser.controlpanel import IMAXUISettings
 from mrs.max.utilities import prettyResponse
+from mrs.max.utilities import IMAXClient
 
 import logging
 
@@ -17,14 +17,8 @@ logger = logging.getLogger('mrs.max')
 def getToken(credentials, grant_type=None):
     user = credentials.get('login').lower()
     password = credentials.get('password')
-    registry = queryUtility(IRegistry)
-    settings = registry.forInterface(IMAXUISettings, check=False)
-    # Pick grant type from settings unless passed as optonal argument
-    effective_grant_type = grant_type is not None and grant_type or settings.oauth_grant_type
 
-    ## TODO: Do we need to ask for a token always?
-    # r = requests.post(settings.oauth_token_endpoint, data=payload, verify=False)
-    maxclient = MaxClient(url=settings.max_server, oauth_server=settings.oauth_server, grant_type=effective_grant_type)
+    maxclient, settings = getUtility(IMAXClient)()
 
     try:
         token = maxclient.getToken(user, password)
@@ -43,7 +37,7 @@ class oauthTokenRetriever(object):
 
     def execute(self, credentials):
         user = credentials.get('login').lower()
-        pm = getToolByName(self.context, "portal_membership")
+        pm = api.portal.get_tool(name='portal_membership')
         member = pm.getMemberById(user)
 
         if user == "admin":
@@ -69,22 +63,12 @@ class maxUserCreator(object):
         if user == "admin":
             return
 
-        registry = queryUtility(IRegistry)
-        settings = registry.forInterface(IMAXUISettings, check=False)
-        # Pick grant type from settings unless passed as optonal argument
-        effective_grant_type = settings.oauth_grant_type
-
-        maxclient = MaxClient(url=settings.max_server, oauth_server=settings.oauth_server, grant_type=effective_grant_type)
+        maxclient, settings = getUtility(IMAXClient)()
         maxclient.setActor(settings.max_restricted_username)
         maxclient.setToken(settings.max_restricted_token)
 
         try:
-            maxclient.people['user'].post()
-
-            # Temporarily subscribe always the user to the default context
-            maxclient.setActor(user)
-            portal_url = getToolByName(self.context, "portal_url").getPortalObject().absolute_url()
-            maxclient.people[user].subscriptions.post(object_url=portal_url)
+            maxclient.people[user].post()
 
             if maxclient.last_response_code == 201:
                 logger.info('MAX user created for user: %s' % user)
@@ -93,6 +77,12 @@ class maxUserCreator(object):
             else:
                 logger.error('Error creating MAX user for user: {}. '.format(user))
                 logger.error(prettyResponse(maxclient.last_response))
+
+            # Temporarily subscribe always the user to the default context
+            maxclient.setActor(user)
+            portal_url = api.portal.get().absolute_url()
+            maxclient.people[user].subscriptions.post(object_url=portal_url)
+
         except:
             logger.error('Could not contact with MAX server.')
             logger.error(prettyResponse(maxclient.last_response))
