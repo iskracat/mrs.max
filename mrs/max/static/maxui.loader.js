b12a2bbc1ef4ee19580faac006f34691d50580be
Carles Bruguera <carlesba@gmail.com>
2014-06-27 11:39:34 +0200

    Ooops

--- mrs/max/static/maxui.loader.js
+++ mrs/max/static/maxui.loader.js
@@ -40,7 +40,7 @@ window._MAXUI.onReady = function() {
 */
 
 (function(d){
-var mui_location = '/demo/++maxui++static/max.ui.min.js'
+var mui_location = '/++maxui++static/max.ui.min.js'
 var mui = d.createElement('script'); mui.type = 'text/javascript'; mui.async = true;
 mui.src = mui_location
 var s = d.getElementsByTagName('script')[0]; s.parentNode.insertBefore(mui, s);
